d8f9b0e451d9af124e8133a1a1a3681e18720a96
Andrewonetap <bobovivan13@gmail.com>
2025-09-20 13:28:08 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int status = 0;
+typedef struct
+{
+    char uid[16]; // идентификатор дрона
+
+    bool is_connected; // подключён ли 0/1
+    int battery_charge; // заряд батареи 0-100%
+
+    float latitude; // широта
+    float longitude; //долгота
+    float altitude; // высота
+
+    enum {
+        DISARMED, ARMING,
+        ARMED, TAKING_OFF,
+        IN_MISSION, LANDING,
+        ERROR
+    } flight_status; // статус дрона
+}drone_unit;
 
 typedef struct
 {
-    int battery_charge; // 0-100%
+    float temperature;
+    float humidity;
+    float wind_speed;
+    bool is_raining;
+}weather_station;
 
-}drone;
+
+typedef struct
+{
+
+}server;
+
+typedef struct
+{
+    char dp_uid[16];
+    int dp_battery_charge;
+
+    drone_unit drone;
+    weather_station weather;
+    // server структура сервера
+
+    char log[256]; // логи
+}droneport;
+
 
 int main()
 {
+    int a = 0;
     while(1)
     {
-        return 1;
+        if(a==10)
+        {
+            status = 1;
+        }
+        a++;
+        return status;
     }
-    return 0;
+    return status;
 }
